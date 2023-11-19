ed654283799eb8a93a492ae3dfa2a318b2e9adf9
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-11-19 11:30:15 +0300

    angular project init

---
+++ old/src/main.ts
@@ -0,0 +1,3 @@
+import { ScClient } from "../ts-sc-client/src";
+
+const client: ScClient = new ScClient("https://192.168.0.105:8000/");
---
+++ scn-redactor/src/components/scn-list-item/scn-list-item.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'scn-list-item',
+    templateUrl: './scn-list-item.component.html',
+    styleUrls: ['./scn-list-item.component.scss'],
+    standalone: true,
+    imports: [CommonModule]
+})
+export class ScnListItemComponent {
+    
+}
